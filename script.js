a41cc37f28beae22e74b20612d743426553023e7
RyanManinga <ryanmaninga@gmail.com>
2024-11-03 20:55:55 +0300

    Forms

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
-
-
+document.getElementById("myForm").addEventListener("submit", async function (e) {
+        e.preventDefault();
+        
+        const formData = new FormData(this);
+        
+        try {
+            const response = await fetch("https://script.google.com/macros/s/AKfycbwkr9qcH2R-I3IcQA4UiqUC-MW25fHAppx7owq8cPIkYMvzFT7xwWjUH_1A0G8zeit_ww/exec", {  // Replace with your web app URL
+                method: "POST",
+                body: formData
+            });
+            
+            const result = await response.json();
+            alert(result.result);
+        } catch (error) {
+            alert("Error submitting form");
+        }
+    });
 
