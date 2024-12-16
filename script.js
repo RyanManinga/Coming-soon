fb9b6f96fb2bb0d691988f7ab12b6e0743819382
RyanManinga <ryanmaninga@gmail.com>
2024-12-16 15:23:11 +0300

    Prevented printing across all pages

--- script.js
+++ script.js
@@ -53,4 +53,11 @@ document.addEventListener('contextmenu', function(e) {
     }
   });
 
+document.addEventListener('keydown', function(event) {
+    
+    if (event.ctrlKey && event.key === 'p') {
+      event.preventDefault(); // Prevent the default print behavior
+      alert('Printing is disabled on this page.');
+    }
+  });
   
